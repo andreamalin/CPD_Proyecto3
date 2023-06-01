ffc0fe10dbf8cce72acaefa93fda0eff4bf5f00a
Requetetin <martinamadog@gmail.com>
2023-06-01 16:16:45 -0600

    MOD - Fixed constant memory

--- hough_constant.cu
+++ hough_constant.cu
@@ -11,15 +11,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
+#include <vector>
 
 #include <cuda.h>
 #include <string.h>
-#include "pgm.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "CImg.h"
+using namespace cimg_library;
 
 #define M_PI 3.14159265358979323846
+#define DEG2RAD (M_PI/180.0f)
 const int degreeInc = 2;
 const int degreeBins = 180 / degreeInc;
 const int rBins = 100;
@@ -29,27 +30,25 @@ const float radInc = degreeInc * M_PI / 180;
 void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc)
 {
   float rMax = sqrt (1.0 * w * w + 1.0 * h * h) / 2;  //(w^2 + h^2)/2, radio max equivalente a centro -> esquina
-  *acc = new int[rBins * degreeBins];            //el acumulador, conteo depixeles encontrados, 90*180/degInc = 9000
-  memset (*acc, 0, sizeof (int) * rBins * degreeBins); //init en ceros
+  *acc = new int[(int)((round(rMax * 2 *180)))];            //el acumulador, conteo depixeles encontrados, 90*180/degInc = 9000
+  memset (*acc, 0, sizeof (int) * (int)((round(rMax * 2 *180)))); //init en ceros
+
   int xCent = w / 2;
   int yCent = h / 2;
-  float rScale = 2 * rMax / rBins;
 
-  for (int i = 0; i < w; i++) //por cada pixel
-    for (int j = 0; j < h; j++) //...
+  for (int x = 0; x < w; x++) //por cada pixel
+    for (int y = 0; y < h; y++) //...
       {
-        int idx = j * w + i;
+        int idx = (y * w) + x;
         if (pic[idx] > 0) //si pasa thresh, entonces lo marca
           {
-            int xCoord = i - xCent;
-            int yCoord = yCent - j;  // y-coord has to be reversed
-            float theta = 0;         // actual angle
-            for (int tIdx = 0; tIdx < degreeBins; tIdx++) //add 1 to all lines in that pixel
+            float xCoord = x - xCent;
+            float yCoord = y - yCent; 
+            //printf("CPU: ID: %i; X: %f, Y: %f", idx, xCoord, yCoord);
+            for (int theta = 0; theta < rMax; theta++)
               {
-                float r = xCoord * cos (theta) + yCoord * sin (theta);
-                int rIdx = (r + rMax) / rScale;
-                (*acc)[rIdx * degreeBins + tIdx]++; //+1 para este radio r y este theta
-                theta += radInc;
+                float distance = ( (xCoord) * cos((float)theta * DEG2RAD)) + ((yCoord) * sin((double)theta * DEG2RAD));
+                (*acc)[ (int)((round(distance + rMax) * 180)) + theta]++; //+1 para este radio distance y este theta
               }
           }
       }
@@ -85,20 +84,20 @@ __global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float
 
   //TODO explicar bien bien esta parte. Dibujar un rectangulo a modo de imagen sirve para visualizarlo mejor
   int xCoord = gloID % w - xCent;
-  int yCoord = yCent - gloID / w;
+  int yCoord = (yCent - gloID / w) * -1;
 
   //TODO eventualmente usar memoria compartida para el acumulador
 
   if (pic[gloID] > 0)
     {
-      for (int tIdx = 0; tIdx < degreeBins; tIdx++)
+      // printf("GPU: ID: %i; X: %i, Y: %i", gloID, xCoord, yCoord);
+      for (int theta = 0; theta < rMax; theta++)
         {
           //DONE utilizar memoria constante para senos y cosenos
-          float r = xCoord * cosf((double)tIdx) + yCoord * sinf((double)tIdx); //probar con esto para ver diferencia en tiempo
+          float distance = (xCoord * cosf((float)theta * DEG2RAD)) + (yCoord * sinf((double)theta * DEG2RAD)); //probar con esto para ver diferencia en tiempo
           // float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
-          int rIdx = (r + rMax) / rScale;
           //debemos usar atomic, pero que race condition hay si somos un thread por pixel? explique
-          atomicAdd (acc + (rIdx * degreeBins + tIdx), 1);
+          atomicAdd(&acc[(int)((round(distance + rMax) * 180)) + theta], 1); //+1 para este radio distance y este theta
         }
     }
 
@@ -113,11 +112,14 @@ int main (int argc, char **argv)
 {
   int i;
 
-  PGMImage inImg (argv[1]);
+  const char* inputFileName = argv[1];
+  const char* outputFileName = argv[2];
+
+  CImg<unsigned char> image(inputFileName);
 
   int *cpuht;
-  int w = inImg.x_dim;
-  int h = inImg.y_dim;
+  int w = image.width();
+  int h = image.height();
 
   // float* d_Cos;
   // float* d_Sin;
@@ -126,7 +128,7 @@ int main (int argc, char **argv)
   // cudaMalloc ((void **) &d_Sin, sizeof (float) * degreeBins);
 
   // CPU calculation
-  CPU_HoughTran(inImg.pixels, w, h, &cpuht);
+  CPU_HoughTran(image.data(), w, h, &cpuht);
 
   // pre-compute values to be stored
   float *pcCos = (float *) malloc (sizeof (float) * degreeBins);
@@ -152,9 +154,8 @@ int main (int argc, char **argv)
   unsigned char *d_in, *h_in;
   int *d_hough, *h_hough;
 
-  h_in = inImg.pixels; // h_in contiene los pixeles de la imagen
-
-  h_hough = (int *) malloc (degreeBins * rBins * sizeof (int));
+  h_in = image.data(); // h_in contiene los pixeles de la imagen
+  h_hough = (int *) malloc (sizeof (int) * (int)((round(rMax * 2 * 180))));
 
   cudaMalloc ((void **) &d_in, sizeof (unsigned char) * w * h);
   cudaMalloc ((void **) &d_hough, sizeof (int) * degreeBins * rBins);
@@ -190,9 +191,52 @@ int main (int argc, char **argv)
 
 
   // Guardando como png
-  const char* filename = "result.png";
-  int stride = w;  // Apunta a escala en grises
-  stbi_write_png(filename, w, h, 1, h_hough, stride);
+  CImg<unsigned char> result_image = image;
+  unsigned char red[] = {255, 0, 0};
+
+  int threshold = 200;  // AJUSTAR SEGUN LO NECESARIO
+  // Guardando los valores arriba del threshold
+
+  float _accu_h = ((sqrt(2.0) * (double)(h>w?h:w)) / 2.0) * 2; 
+  std::vector<std::pair<int, int>> indices;
+  for(int r=0;r<_accu_h;r++) {
+    for(int t=0;t<180;t++) {
+      if((int)cpuht[(r*180) + t] >= threshold) {
+        indices.push_back(std::make_pair(r, t));
+      }
+    }
+  }
+
+    
+  for (const auto& index : indices) {
+    int r = index.first;
+    int t = index.second;
+    int x1, y1, x2, y2;
+    x1 = y1 = x2 = y2 = 0;
+
+    if(t >= 45 && t <= 135)
+    {
+      //y = (r - x cos(t)) / sin(t)
+      x1 = 0;
+      y1 = ((double)(r-(_accu_h/2)) - ((x1 - (w/2) ) * cos(t * DEG2RAD))) / sin(t * DEG2RAD) + (h / 2);
+      x2 = w - 0;
+      y2 = ((double)(r-(_accu_h/2)) - ((x2 - (w/2) ) * cos(t * DEG2RAD))) / sin(t * DEG2RAD) + (h / 2);
+    }
+    else
+    {
+      //x = (r - y sin(t)) / cos(t);
+      y1 = 0;
+      x1 = ((double)(r-(_accu_h/2)) - ((y1 - (h/2) ) * sin(t * DEG2RAD))) / cos(t * DEG2RAD) + (w / 2);
+      y2 = h - 0;
+      x2 = ((double)(r-(_accu_h/2)) - ((y2 - (h/2) ) * sin(t * DEG2RAD))) / cos(t * DEG2RAD) + (w / 2);
+    }
+
+    result_image.draw_line(x1, y1, x2, y2, red);
+  }
+
+  // Display or save the resulting image with detected lines
+  result_image.display();  // Display the image using CImg's built-in display function
+  result_image.save(outputFileName);  // Save the image with detected lines to a file
 
   
   cudaEventDestroy(start);
